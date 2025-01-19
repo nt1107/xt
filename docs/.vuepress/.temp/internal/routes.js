c9e6dabb18af5df8af0d738543b0c30e004f5714
junfengTan <junfeng.tan@izhijue.com>
2025-01-19 21:41:59 +0800

    chore: package dev

--- docs/.vuepress/.temp/internal/routes.js
+++ docs/.vuepress/.temp/internal/routes.js
@@ -7,3 +7,20 @@ export const routes = Object.fromEntries([
   ["/post/", { loader: () => import(/* webpackChunkName: "post_index.html" */"E:/private/xt/docs/.vuepress/.temp/pages/post/index.html.js"), meta: {"title":"博客文章列表"} }],
   ["/404.html", { loader: () => import(/* webpackChunkName: "404.html" */"E:/private/xt/docs/.vuepress/.temp/pages/404.html.js"), meta: {"title":""} }],
 ]);
+
+if (import.meta.webpackHot) {
+  import.meta.webpackHot.accept()
+  if (__VUE_HMR_RUNTIME__.updateRoutes) {
+    __VUE_HMR_RUNTIME__.updateRoutes(routes)
+  }
+  if (__VUE_HMR_RUNTIME__.updateRedirects) {
+    __VUE_HMR_RUNTIME__.updateRedirects(redirects)
+  }
+}
+
+if (import.meta.hot) {
+  import.meta.hot.accept(({ routes, redirects }) => {
+    __VUE_HMR_RUNTIME__.updateRoutes(routes)
+    __VUE_HMR_RUNTIME__.updateRedirects(redirects)
+  })
+}
--- docs/.vuepress/.temp/pages/post/firstPost.html.js
+++ docs/.vuepress/.temp/pages/post/firstPost.html.js
@@ -1,3 +1,16 @@
 import comp from "E:/private/xt/docs/.vuepress/.temp/pages/post/firstPost.html.vue"
 const data = JSON.parse("{\"path\":\"/post/firstPost.html\",\"title\":\"第一篇博客\",\"lang\":\"en-US\",\"frontmatter\":{\"title\":\"第一篇博客\"},\"headers\":[{\"level\":2,\"title\":\"子标题\",\"slug\":\"子标题\",\"link\":\"#子标题\",\"children\":[]},{\"level\":2,\"title\":\"子二\",\"slug\":\"子二\",\"link\":\"#子二\",\"children\":[{\"level\":3,\"title\":\"子子标题\",\"slug\":\"子子标题\",\"link\":\"#子子标题\",\"children\":[]}]}],\"git\":{\"updatedTime\":1737289715000,\"contributors\":[{\"name\":\"junfengTan\",\"username\":\"junfengTan\",\"email\":\"junfeng.tan@izhijue.com\",\"commits\":1,\"url\":\"https://github.com/junfengTan\"}]},\"filePathRelative\":\"post/firstPost.md\"}")
 export { comp, data }
+
+if (import.meta.webpackHot) {
+  import.meta.webpackHot.accept()
+  if (__VUE_HMR_RUNTIME__.updatePageData) {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  }
+}
+
+if (import.meta.hot) {
+  import.meta.hot.accept(({ data }) => {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  })
+}
